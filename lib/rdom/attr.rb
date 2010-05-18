f3631d96114dd758427ad128d7e99fa32f82b141
Sven Fuchs <svenfuchs@artweb-design.de>
2010-05-18 22:27:03 +0200

    attr#nodeValue

--- lib/rdom/attr.rb
+++ lib/rdom/attr.rb
@@ -1,6 +1,6 @@
 module RDom
   module Attr
-    properties :name, :value
+    properties :name, :value, :nodeValue
 
     # TODO does not work with Properties unconditionally uses libxml_read_attribute
     def name
@@ -9,6 +9,10 @@ module RDom
 
     def value
       super
+    end
+
+    def nodeValue
+      value
     end
   end
 end
