855c49ea4bb4f9857f78754764dedf5cf776a9e2
Sven Fuchs <svenfuchs@artweb-design.de>
2010-06-05 15:11:49 +0200

    mokey patch nokogiri pp so that it uses ruby_class

--- lib/nokogiri/xml_pp_node_patch.rb
+++ lib/nokogiri/xml_pp_node_patch.rb
@@ -14,4 +14,39 @@ module Nokogiri::XML::PP::Node
     }.join ' '
     "#<#{self.ruby_class.name}:#{sprintf("0x%x", object_id)} #{attributes}>"
   end
+
+  def pretty_print(pp)
+    nice_name = self.ruby_class.name.split('::').last
+    pp.group(2, "#(#{nice_name}:#{sprintf("0x%x", object_id)} {", '})') do
+
+      pp.breakable
+      attrs = inspect_attributes.map { |t|
+        [t, send(t)] if respond_to?(t)
+      }.compact.find_all { |x|
+        if x.last
+          if [:attribute_nodes, :children].include? x.first
+            !x.last.empty?
+          else
+            true
+          end
+        end
+      }
+
+      pp.seplist(attrs) do |v|
+        if [:attribute_nodes, :children].include? v.first
+          pp.group(2, "#{v.first.to_s.sub(/_\w+$/, 's')} = [", "]") do
+            pp.breakable
+            pp.seplist(v.last) do |item|
+              pp.pp item
+            end
+          end
+        else
+          pp.text "#{v.first} = "
+          pp.pp v.last
+        end
+      end
+      pp.breakable
+
+    end
+  end
 end
