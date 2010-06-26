47dc968e712b0be7b6d7ee57bcc51e4a66694bbb
Sven Fuchs <svenfuchs@artweb-design.de>
2010-06-26 15:22:28 +0200

    stop defining attr_readers on Attributes

--- lib/rdom/attributes.rb
+++ lib/rdom/attributes.rb
@@ -40,34 +40,7 @@ module RDom
 
     # returns the indexth item in the map. If index is greater than or equal to the number of nodes in the map, this returns null
     def item(index)
-      values.each_with_index { |item, ix| return item if index == ix } && nil
-    end
-    
-    def respond_to?(name)
-      attributes.key?(name) || attributes.respond_to?(name) || super
-    end
-    
-    def method_missing(name, *args)
-      return attributes[name] if attributes.key?(name)
-      return attributes.send(name, *args) if attributes.respond_to?(name)
-      super
-    end
-    
-    # hmm, therubyracer expects methods to be actually defined
-    def method(name)
-      define_attribute_methods(name) unless ruby_class.method_defined?(name)
-      super
-    end
-    
-    def attribute_methods
-      @attribute_methods ||= Module.new.tap { |m| self.extend(m) }
-    end
-    
-    def define_attribute_methods(name)
-      attribute_methods.module_eval <<-rb
-        def #{name}; attributes['#{name}']; end
-        def #{name}=(value); attributes['#{name}'] = value; end
-      rb
+      attributes.values.each_with_index { |item, ix| return item if index == ix } && nil
     end
   end
 end
